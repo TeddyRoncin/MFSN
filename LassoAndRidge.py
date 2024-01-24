0e8d3cbdfe6c71e78ff790c7459d23e93e070f94
Teddy Roncin <teddy.roncin@proton.me>
2024-01-24 10:54:09 +0100

    Update LassoAndRidge.py

--- LassoAndRidge.py
+++ LassoAndRidge.py
@@ -1,7 +1,10 @@
 import random
+
+from sklearn.model_selection import GridSearchCV
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import Ridge, Lasso
 from sklearn.metrics import mean_squared_error
+
 
 dimension_data = [(random.random() * 0.3, random.random() * 100) for i in range(100)]
 
@@ -19,14 +22,19 @@ def generate_data(count):
     return X, y
 
 
-X, y = generate_data(10000) # DO NOT PUT 1 MILLION, IT WILL MAKE YOUR COMPUTER CRASH
+X, y = generate_data(10000)  # DO NOT PUT 1 MILLION, IT WILL MAKE YOUR COMPUTER CRASH
 
+alpha_values = {"alpha": [0.0001, 0.001, 0.01, 0.1, 1, 10, 100]}
 scaler = StandardScaler()
 X = scaler.fit_transform(X)
-ridge = Ridge()
-ridge.fit(X, y)
-lasso = Lasso()
-lasso.fit(X, y)
+
+model = GridSearchCV(Ridge(), alpha_values, scoring="neg_mean_squared_error")
+model.fit(X, y)
+ridge = model.best_estimator_
+
+model = GridSearchCV(Lasso(), alpha_values, scoring="neg_mean_squared_error")
+model.fit(X, y)
+lasso = model.best_estimator_
 
 X_test, y_test = generate_data(1000)
 X_test = scaler.transform(X_test)
@@ -37,12 +45,10 @@ y_test_predict_ridge = ridge.predict(X_test)
 y_predict_lasso = lasso.predict(X)
 y_test_predict_lasso = lasso.predict(X_test)
 
-print("Score ridge")
+print("SCR ridge")
 print("\tTrain : " + str(mean_squared_error(y, y_predict_ridge) * len(y)))
 print("\tTest : " + str(mean_squared_error(y_test, y_test_predict_ridge) * len(y_test)))
 
-print("Score lasso")
+print("SCR lasso")
 print("\tTrain : " + str(mean_squared_error(y, y_predict_lasso) * len(y)))
 print("\tTest : " + str(mean_squared_error(y_test, y_test_predict_lasso) * len(y_test)))
-
-
